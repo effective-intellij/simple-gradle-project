f25c967f9ce18521c166319a96efd1fad580ad0f
Trisha Gee <trisha.gee@gmail.com>
2021-01-28 13:40:07 +0100

    Basic application class to run with Gradle

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    private static final String APPLICATION_NAME = "My Gradle Application";
+
+    public static void main(String[] args) {
+        System.out.println("Hello from " + APPLICATION_NAME);
+    }
+}
